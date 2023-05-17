97a432880e66543d70f414f883b56f6b0a427bce
atkarolis <atkarolis@gmail.com>
2023-05-17 10:45:34 +0300

    header structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/UI/Header';
+import Home from './components/Pages/Home';
 
 const App = () => {
   return (
     <>
+    <Header />
+    <Routes>
+        <Route index element={<Home />}/>
+    </Routes>
     </>
   );
 }
---
+++ src/components/UI/Header.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import styled from "styled-components";
+import logo from '../../resources/ask.svg';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src={logo} alt="ask-logo" />
+      </div>
+      <nav>
+        <ul>
+          <li><NavLink to='/'>HOME</NavLink></li>
+          <li><NavLink to='/'>ABOUT</NavLink></li>
+          <li><NavLink to='/'>LOG IN</NavLink></li>
+          <li><NavLink to='/'>REGISTER</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+ 
+export default Header;
