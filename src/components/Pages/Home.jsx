19b42891ecdf421290ee75529cebbe79d0017e44
atkarolis <atkarolis@gmail.com>
2023-05-17 18:31:59 +0300

    homepage basic styles added

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,9 +1,77 @@
 import { NavLink } from 'react-router-dom';
+import styled from "styled-components";
 //<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+const StyledMain = styled.main`
+  min-height: calc(100vh - 320px);
+  display: flex;
+  aside{
+    border-width: 7px;
+    border-color: black;
+    border-style: double;
+    border-radius: 30px;
+    margin: 20px;
+    padding: 10px 35px 70px 5px;
+    height: 100%;
+  }
+  .MAIN {
+    padding: 20px 200px 20px 0px;
+    width: 100%;
+    > section {
+      display: flex;
+      padding-bottom: 20px;
+      width: 100%;
+      .search-container {
+        width: 100%;
+        display: flex;
+        input {
+        width: 70%;
+        margin-left: 105px;
+        }
+        button {
+          cursor: pointer;
+          margin-right: 10px;
+        }
+      }
+      button {
+        cursor: pointer;
+      }
+    }
+    article {
+      > section {
+        display: flex;
+        .vote-container {
+          display: flex;
+          flex-direction: column;
+          margin: 20px;
+          button {
+            cursor: pointer;
+          }
+        }
+        .question-container {
+          border: 1px solid black;
+          border-radius: 10px;
+          width: 100%;
+          .details {
+            display: flex;
+            justify-content: space-between;
+          }
+          .categories {
+            display: flex;
+            justify-content: flex-end;
+            span {
+              border: 1px solid black;
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const Home = () => {
   return ( //aside ir section iškelti kaip UI komponentą?
-    <>
+    <StyledMain>
       <aside>
         <ul>
           <li><NavLink to='/'>Home</NavLink></li>
@@ -19,37 +87,39 @@ const Home = () => {
           <li>About</li>
         </ul>
       </aside>
-      <main>
+      <div className='MAIN'>
         <section>
           <div className='search-container'>
             <input type="text" placeholder='Search questions'/>
-            <button type="submit"><i class="fa fa-search"></i></button>
+            <button type="submit"><i class="fa fa-search">S</i></button>
+            <button>Filter</button>
           </div>
-          <button>Filter</button>
+          <button>Ask Your Question</button>
         </section>
         <article>
-          <button>Ask Your Question</button>
           <section>
-            <h4>Q title</h4>
-            <div className='question-container'>
-              <p>QUESTION QUESTION Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus tempore nesciunt dolor quis est explicabo doloremque voluptatum. Et, nam libero molestias soluta, sunt eum totam sed maxime molestiae ea porro.</p>
-              <span>User-name</span>
-              <span>Date Created/Edited</span>
-              <div>
-                <span>Category1</span>
-                <span>Category2</span>
-              </div>
-            </div>
             <div className='vote-container'>
               <button>^</button>
               <span>votes</span>
               <button>v</button>
               <span>ans count</span>
             </div>
+            <div className='question-container'>
+              <h4>Q title</h4>
+              <p>QUESTION QUESTION Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus tempore nesciunt dolor quis est explicabo doloremque voluptatum. Et, nam libero molestias soluta, sunt eum totam sed maxime molestiae ea porro.</p>
+              <div className='details'>
+                <span>User-name</span>
+                <span>Date Created/Edited</span>
+              </div>
+              <div className='categories'>
+                <span>Category1</span>
+                <span>Category2</span>
+              </div>
+            </div>
           </section>
         </article>
-      </main>
-    </>
+      </div>
+    </StyledMain>
   );
 }
  
--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -3,15 +3,15 @@ import styled from "styled-components";
 
 const StyledFooter = styled.footer`
   height: 200px;
+  padding-right: 200px;
   background-color: #000000;
   color: #FFFFFF;
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: space-between;
   .links-div {
-    padding-top: 25px;
+    padding: 25px 0px 0px 250px;
     display: flex;
-    gap: 80px;
+    gap: 30px;
   }
   ul {
     list-style-type: none;
@@ -22,32 +22,34 @@ const StyledFooter = styled.footer`
     color: #FFFFFF
   }
   .credits {
-    position: absolute;
-    bottom: 10px;
-    right: 200px;
-    color: red; //adjust the color and font-size (?) later
+    padding-top: 130px;
+    font-size: 14px;
+    color: #FFFFFF;
+    p {
+      margin: 0;
+    }
   }
 `
 
 const Footer = () => {
   return (
     <StyledFooter>
-      <div className='links-div'>
-        <ul>
-          <li><NavLink to='/'>Home</NavLink></li>
-          <li><NavLink to='/'>About</NavLink></li>
-        </ul>
-        <ul>
-          <li><a href="https://www.facebook.com/" target="_blank">Facebook</a></li>
-          <li><a href="https://www.instagram.com/" target="_blank">Instagram</a></li>
-          <li><a href="https://www.reddit.com/" target="_blank">Reddit</a></li>
-          <li><a href="https://discord.com/" target="_blank">Discord</a></li>
-        </ul>
-      </div>
-      <div className='credits'>
-        <p>© 2023 Ask Anything Group</p>
-        <p>developed by atkarolis</p>
-      </div>
+        <div className='links-div'>
+          <ul>
+            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink to='/'>About</NavLink></li>
+          </ul>
+          <ul>
+            <li><a href="https://www.facebook.com/" target="_blank">Facebook</a></li>
+            <li><a href="https://www.instagram.com/" target="_blank">Instagram</a></li>
+            <li><a href="https://www.reddit.com/" target="_blank">Reddit</a></li>
+            <li><a href="https://discord.com/" target="_blank">Discord</a></li>
+          </ul>
+        </div>
+        <div className='credits'>
+          <p>© 2023 Ask Anything Group</p>
+          <p>developed by atkarolis</p>
+        </div>
     </StyledFooter>
   );
 }
