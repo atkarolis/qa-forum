2c79e7fe0520d332b8bf0e213cf0fac00c197c10
atkarolis <atkarolis@gmail.com>
2023-05-17 22:08:07 +0300

    users id was connected with quesions

--- src/components/UI/Molecules/Question.jsx
+++ src/components/UI/Molecules/Question.jsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
+import { useContext } from 'react';
+import UsersContext from "../../../contexts/UsersContext";
 
-const Question = () => {
+const Question = ({ data }) => {
+
+  const { users } = useContext(UsersContext);
+  const user = users.find(el => el.id === data.user_id);
+
   return (
     <article>
       <div className='vote-container'>
@@ -13,7 +19,7 @@ const Question = () => {
         <h4>{data.title}</h4>
         <p>{data.question}</p>
         <div className='details'>
-          <span>ČIA USER NAME</span>
+          <span>{user.username}</span>
           <span>{data.edited}</span>
         </div>
         <div className='categories'>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { QuestionsProvider } from './contexts/QuestionsContext';
+import { UsersProvider } from './contexts/UsersContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <QuestionsProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </QuestionsProvider>
+    <UsersProvider>
+        <QuestionsProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </QuestionsProvider>
+    </UsersProvider>
 );
