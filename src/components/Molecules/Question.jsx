20d7f803b3d2fc2fef39afd79c2e15db792c0d28
atkarolis <atkarolis@gmail.com>
2023-05-20 14:49:02 +0300

    question delete button created

--- src/components/Molecules/Question.jsx
+++ src/components/Molecules/Question.jsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import { useContext } from 'react';
 import UsersContext from "../../contexts/UsersContext";
+import QuestionsContext from "../../contexts/QuestionsContext";
 
 const Question = ({ data }) => {
 
-  const { users } = useContext(UsersContext);
+  const { users, currentUser } = useContext(UsersContext);
   const user = users.find(el => el.id === data.user_id);
-  
+  const { setQuestions, QuestionsActionTypes } = useContext(QuestionsContext)
+
   return (
     <article>
       <div className='vote-container'>
@@ -21,6 +23,19 @@ const Question = ({ data }) => {
         <div className='details'>
           <span>{user.username}</span>
           <span>{data.edited}</span>
+        </div>
+        <div>
+          {
+            currentUser &&
+            <button
+            onClick={() => setQuestions({
+              type: QuestionsActionTypes.delete,
+              id: data.id
+            })}
+            >
+              Delete
+            </button>
+          }
         </div>
       </div>
     </article>
