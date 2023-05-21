ec61b50a0cf0ac6c697b2239eeeb926ecc37e69b
atkarolis <atkarolis@gmail.com>
2023-05-21 23:28:59 +0300

    answer page route added

---
+++ src/components/Pages/Answer.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState, useContext} from "react";
+// import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import QuestionsContext from "../../contexts/QuestionsContext";
+
+
+const Answer = () => {
+
+  // const navigate = useNavigate();
+
+  return (
+    <main>
+    </main>
+  );
+}
+ 
+export default Answer;
