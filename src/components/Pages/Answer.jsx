125f774dd98fd03ababb62a91be1a87398d31d2f
atkarolis <atkarolis@gmail.com>
2023-05-21 23:56:51 +0300

    answers context created

--- src/components/Pages/Answer.jsx
+++ src/components/Pages/Answer.jsx
@@ -2,14 +2,24 @@ import { useEffect, useState, useContext} from "react";
 // import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import QuestionsContext from "../../contexts/QuestionsContext";
+import UsersContext from "../../contexts/UsersContext";
 
 
-const Answer = () => {
+const Answer = ({ data }) => {
+
+  const { questions } = useContext(QuestionsContext);
+  const { users } = useContext(UsersContext);
+  const user = users.find(el => el.id === data.user_id);
 
   // const navigate = useNavigate();
 
   return (
     <main>
+      <div>
+        <h4>{questions.title}</h4>
+        <p>{questions.question}</p>
+        <p>by {user.username}</p>
+      </div>
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { QuestionsProvider } from './contexts/QuestionsContext';
 import { UsersProvider } from './contexts/UsersContext';
+import { AnswersProvider } from './contexts/AnswersContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <UsersProvider>
-        <QuestionsProvider>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </QuestionsProvider>
-    </UsersProvider>
+    <AnswersProvider>
+        <UsersProvider>
+            <QuestionsProvider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </QuestionsProvider>
+        </UsersProvider>
+    </AnswersProvider>
 );
