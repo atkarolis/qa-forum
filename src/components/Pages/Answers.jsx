529136690494f42eb191c2f865934a52d9bb38cb
atkarolis <atkarolis@gmail.com>
2023-05-22 19:00:22 +0300

    answers page and route added

---
+++ src/components/Pages/Answers.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+  min-height: calc(100vh - 320px);
+  padding-top: 120px;
+`
+
+const Answers = () => {
+  return (
+    <StyledMain>
+      <div>
+        <p>ddd</p>
+      </div>
+    </StyledMain>
+  );
+}
+ 
+export default Answers;
