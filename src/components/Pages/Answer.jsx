b6090d3e1b0c76a3cd7328d47c94aa5c136964e1
atkarolis <atkarolis@gmail.com>
2023-05-22 09:32:28 +0300

    minor changes

--- src/components/Pages/Answer.jsx
+++ src/components/Pages/Answer.jsx
@@ -3,22 +3,24 @@ import { useEffect, useState, useContext} from "react";
 import { Link } from 'react-router-dom';
 import QuestionsContext from "../../contexts/QuestionsContext";
 import UsersContext from "../../contexts/UsersContext";
+import AnswersContext from "../../contexts/AnswersContext";
 
+const { data } = useContext(QuestionsContext);
+const question = questions.find(el => el.id === data.id) //currentQuestion reik pasiimt kaip ir currentUser buvo
 
 const Answer = ({ data }) => {
 
-  const { questions } = useContext(QuestionsContext);
-  const { users } = useContext(UsersContext);
-  const user = users.find(el => el.id === data.user_id);
+  // const { users } = useContext(UsersContext);
+  // const user = users.find(el => el.id === data.user_id);
 
   // const navigate = useNavigate();
 
   return (
     <main>
       <div>
-        <h4>{questions.title}</h4>
-        <p>{questions.question}</p>
-        <p>by {user.username}</p>
+        <h4>{data.title}</h4>
+        <p>{data.question}</p>
+        {/* <p>by {user.username}</p> */}
       </div>
     </main>
   );
