27932f8804ae5f3cd1a081937bcb00cd25a9e924
atkarolis <atkarolis@gmail.com>
2023-05-22 23:13:14 +0300

    questions passed to answers

--- src/components/Pages/Answers.jsx
+++ src/components/Pages/Answers.jsx
@@ -1,4 +1,7 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import AnswersContext from "../../contexts/AnswersContext";
+import { useLocation } from "react-router-dom";
 
 const StyledMain = styled.main`
   min-height: calc(100vh - 320px);
@@ -6,10 +9,22 @@ const StyledMain = styled.main`
 `
 
 const Answers = () => {
+
+  const { answers } = useContext(AnswersContext);
+  // const { questions } = useContext(QuestionsContext);
+  const question = useLocation().state;
+  // const question = questions.find(el => el.id === this.data.id)
+
+  console.log(question);
   return (
     <StyledMain>
       <div>
-        <p>ddd</p>
+        <h4>
+          {question.title}
+        </h4>
+        <p>
+          {question.question}
+        </p>
       </div>
     </StyledMain>
   );
